19ae40bcce2306a7c77a3e3b524a2f40609ba8c9
memecorn <iammeghana@proton.me>
2025-12-01 21:08:44 +0530

    take more than one value and show the list

--- lists&dicts/todo.py
+++ lists&dicts/todo.py
@@ -1,10 +1,23 @@
-users_task = input("enter task")
-task_status = input("done?")
-#want the keys to be fixed
-task_dict = {"task": None, "done": None} #had to ask "i want to set the key but value for those 2 keys will be suer inputed" 
-task_dict["task"] = users_task
-task_dict["done"] = task_status
-
+number_of_tasks = int(input("enter number of tasks"))
+task_dict = {"task": None, "done": None}
 todo_list = []
-todo_list.append(task_dict)
+for i in range(number_of_tasks): 
+    
+    users_task = input("enter task")
+    task_status = input("done?")
+    task_dict["task"] = users_task 
+    task_dict["done"] = task_status
+    
+    todo_list.append(task_dict) #all n indices point to the same object since the same task_dict is getting updated each iteration
+
+
 print(f"to-do:{todo_list}")
+
+'''
+1st iteration for index 0 takes study and nope as values of dict and appends it to the list
+to-do:[{'task': 'study', 'done': 'nope'}]
+2nd iteration for index 1 takes garden and yep as values and appends it to the list. 
+since we modified the SAME dict all the exisiting instances also gets modified with this value as well
+todo_list = [task_dict, task_dict]
+so now list becomes to-do:[{'task': 'garden', 'done': 'yep'}, {'task': 'garden', 'done': 'yep'}
+'''
