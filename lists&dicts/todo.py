0c1eba85c9c37223435329e9641dd237d35c4850
memecorn <iammeghana@proton.me>
2025-12-04 23:39:24 +0530

    added interactive menu to let user choose the action

--- lists&dicts/todo.py
+++ lists&dicts/todo.py
@@ -1,23 +1,41 @@
 
-number_of_tasks = int(input("Enter number of tasks: "))
-
 todo_list = []
-for i in range(number_of_tasks):#we are entering inside each dicts
-    task_dict = {}
-    users_task = input("enter task")
-    task_dict["task"] = users_task
-    task_dict["done"] = False
+
+def show_todo():
+    for item in todo_list:#each item is a dict, so for each item we can access the value of specific key
+        print("\nTask:", item["task"], "| Done:", item["done"])
+
+while True:
+    print("\n=== MENU ===")
+    print("1. Add Task")
+    print("2. Mark task as done")
+    print("3. Delete item")
+    print("4. Exit")
     
-    todo_list.append(task_dict)
-print(f"to-do:{todo_list}")
-
-delete_task = input("enter task to delete") #we are entering the loop
-todo_list = [d for d in todo_list if d["task"] != delete_task] 
-print(f"to-do:{todo_list}") 
-
-update_status_task = input("enter task you have completed")
-for d in todo_list:
-    if d["task"] == update_status_task:
-        d["done"] = True
+    choice = input("\nEnter your choice: ")
+    
+    if choice == "1":
+        users_task = input("\nEnter task: ")
+        task_dict = {"task": users_task, "done": False}
+        todo_list.append(task_dict)
+        show_todo()
         
-print(f"to-do:{todo_list}")
+    elif choice == "2":
+        update_status_task = input("\nEnter task you have completed: ")
+        for d in todo_list:
+            if d["task"] == update_status_task:
+                d["done"] = True
+        show_todo()
+                
+    elif choice == "3":
+        delete_task = input("\nEnter task to delete: ") 
+        todo_list = [d for d in todo_list if d["task"] != delete_task]
+        show_todo()
+        
+    elif choice =="4":
+        print("\nlooks like you are done for now!")
+        show_todo()
+        break
+    
+    else:
+        print("\npls enter a number 1 through 5")
