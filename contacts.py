4745690a735f2fe4387a422f6ccc45baede0642e
memecorn <iammeghana@proton.me>
2025-12-04 23:57:51 +0530

    get one name and number from user

---
+++ contacts.py
@@ -0,0 +1,9 @@
+contact_name = input("enter contact name")
+contact_number = input("enter number")
+
+contact_details = {contact_name : contact_number}
+
+contact_tag = input("enter tag")
+contact_details["tag"] = contact_tag
+
+print(contact_details)
