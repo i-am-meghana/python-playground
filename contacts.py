dbfdbefdda6a2f148d00af8f02730cdc69a15057
memecorn <iammeghana@proton.me>
2025-12-04 23:59:57 +0530

    added tagging and entering multiple contacts

--- contacts.py
+++ contacts.py
@@ -1,9 +1,14 @@
-contact_name = input("enter contact name")
-contact_number = input("enter number")
-
-contact_details = {contact_name : contact_number}
-
-contact_tag = input("enter tag")
-contact_details["tag"] = contact_tag
-
-print(contact_details)
+n = int(input("how many contacts are you looking to add this time?"))
+contact_book = []
+for i in range(n):
+    contact_name = input("enter contact name")
+    contact_number = input("enter number")
+    
+    contact_details = {contact_name : contact_number}
+    
+    contact_tag = input("enter tag")
+    contact_details["tag"] = contact_tag
+    
+    contact_book.append(contact_details)
+    
+    print(contact_book)
